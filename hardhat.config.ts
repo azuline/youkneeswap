53124425e96908b7f865f86a8d80bbb042198445
blissful <blissful@sunsetglow.net>
2021-11-28 13:19:11 -0500

    add typechain type generation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,11 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 
-export default {
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
   solidity: "0.8.4",
 };
+
+export default config;
